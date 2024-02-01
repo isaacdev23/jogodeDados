70abb209a3973e22526febe98def128477fac6d6
Aluno <Aluno@SNCTM202LABF15>
2024-01-31 21:09:13 -0300

    Adicionar arquivos de projeto.

---
+++ appDados/jogodeDados.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace appDados
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class jogodeDados : ContentPage
+    {
+        public jogodeDados()
+        {
+            InitializeComponent();
+            Random r = new Random();
+            int n = r.Next(1, 6);
+            if (n == 1)
+            {
+                imgDado.Source = "lado1";
+            }
+            if (n == 2) { }
+            {
+                imgDado.Source = "lado2";
+            }
+            if(n == 3) {
+
+                imgDado.Source = "lado3";
+            }
+            if(n == 4) {
+
+                imgDado.Source = "lado4";
+            }
+            if(n == 5)
+            {
+                imgDado.Source = "lado5";
+               
+            }
+            if(n == 6)
+            {
+                imgDado.Source = "lado6";
+            }
+        }
+
+        private async void btVoltar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
